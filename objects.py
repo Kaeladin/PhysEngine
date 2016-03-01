5425f441e2b64129faadfd51c76030770559ba14
Keene Chin <trix1011@gmail.com>
2016-03-01 01:49:36 -0600

    WALLS

--- objects.py
+++ objects.py
@@ -23,14 +23,34 @@ class KinematicMass(PointMass):
         print "Posx: %.3f, Velx: %.3f, Accx: %.3f, Posy: %.3f, Vely: %.3f, Accy: %.3f" % self.kinematics()
 
 
+class Robot(KinematicMass):
+    def push(self, x, y):
+        self.ax=x
+        self.ay=y
+
+
 def main():
     ts = 0.015
-    bot1 = KinematicMass(10, 5, 5, 4, 4, 1, -2)
+    bot1 = Robot(10, 5, 5, 4, 4, 1, -2)
+
+    xmax=100
+    ymax=100
+    xmin=0
+    ymin=0
+
     for j in range(5000):
+        if j==1000:
+            bot1.push(0,0)
+
         bot1.vx +=  bot1.ax * ts
         bot1.x  +=  bot1.vx * ts
         bot1.vy +=  bot1.ay * ts
         bot1.y  +=  bot1.vy * ts
+        
+        if(bot1.x < xmin) or (bot1.x > xmax):
+            bot1.vx = bot1.vx * -0.9
+        if(bot1.y < ymin) or (bot1.y > ymax):
+            bot1.vy = bot1.vy * -0.9
         bot1.display()   
         time.sleep(ts)
 main()
